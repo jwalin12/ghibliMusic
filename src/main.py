8be8e537dc2c45aa1dce2f694c307811b6d2305a
chancetran <chancetran98@gmail.com>
2020-12-22 20:15:11 -0800

    basic implementation of main.py and network.py
    main.py
      Implemented train_step and test_step for neural net execution.
      Executes basic training and testing (loss and accuracy metrics).
    
    network.py
      Implemented a neural network drawing influence from Skuli.
      Uses tensorflow architecture.
        STILL IN DEVELOPMENT(?).

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
-""" The file used for agent execution.
+# Local import
+from network import MusicGenerator
 
-...
+# Third-party import
+import tensorflow as tf
+
+""" The file used for agent execution.
+    
+    ...
 
 Runs:
   Training
@@ -8,4 +14,72 @@ Runs:
   ...
 """
 
-# INSERT CODE HERE.
+# NEURAL NETWORK EXECUTION HELPER FUNCTIONS.
+@tf.function
+def train_step(X, Y):
+    with tf.GradientTape() as tape:
+        # training=True is only needed if there are layers with different
+        # behavior during training versus inference (e.g. Dropout).
+        predictions = model(X, training=True)
+        loss = loss_object(Y, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(Y, predictions)
+
+@tf.function
+def test_step(X, Y):
+    # training=False is only needed if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    predictions = model(X, training=False)
+    t_loss = loss_object(Y, predictions)
+
+    test_loss(t_loss)
+    test_accuracy(Y, predictions)
+
+
+
+
+
+# PRE-EXECUTION.
+model = MusicGenerator(..., ...)
+
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+optimizer = tf.keras.optimizers.Adam()
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+
+
+# MAIN EXECUTION.
+EPOCHS = 5
+
+train_ds    = ...
+test_ds     = ...
+
+for epoch in range(EPOCHS):
+    # Reset the metrics at the start of the next epoch
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+    test_loss.reset_states()
+    test_accuracy.reset_states()
+
+    for X, Y in train_ds:
+        train_step(X, Y)
+
+    for test_X, test_Y in test_ds:
+        test_step(test_X, test_Y)
+
+    print(
+        f'Epoch {epoch + 1}, '
+        f'Loss: {train_loss.result()}, '
+        f'Accuracy: {train_accuracy.result() * 100}, '
+        f'Test Loss: {test_loss.result()}, '
+        f'Test Accuracy: {test_accuracy.result() * 100}'
+    )
--- src/network.py
+++ src/network.py
@@ -1,9 +1,77 @@
-""" The tensorflow neural network architecture.
+# Local import
+# ...
 
-...
+# Third party import
+import tensorflow as tf
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Activation, Conv2D, Dense, Dropout, Flatten, LSTM
 
-Uses:
-  LSTM
-"""
+class MusicGenerator(Model):
+    """The tensorflow neural network architecture.
 
-# INSERT CODE HERE.
+    ...
+
+    Uses:
+      LSTM
+    """
+
+    def __init__(self, network_input, n_vocab):
+        """A tensorflow model that uses multiple LSTMs to generate musical 
+        output. 
+
+        ...
+
+        WIP: Inspiration by Skuli's architecture at the moment.
+
+        Source:
+        Towards Data Science - How to Generate Music using LSTM  Neural
+        Networks in Keras by Sigurour Skuli
+
+        Arguments:
+            None
+        """
+
+        super(MusicGenerator, self).__init__()
+
+        self.lstm1 = LSTM(
+            256,
+            input_shape=(network_input.shape[1], network_input.shape[2]),
+            return_sequences=True,
+        )
+        self.dropout1 = Dropout(0.3)
+        self.lstm2 = LSTM(
+            512,
+            return_sequences=True,
+        )
+        self.dropout2 = Dropout(0.3)
+        self.lstm3 = LSTM(
+            256
+        )
+        self.dense1 = Dense(256)
+        self.dropout3 = Dropout(0.3)
+        self.dense2 = Dense(n_vocab)
+
+        self.activation1 = Activation('softmax')
+
+
+    def call(self, x):
+        """Executes the neural network.
+
+        ...
+
+        Arguments:
+        x   : The input.
+        """
+
+        x = self.lstm1(x)
+        x = self.dropout1(x)
+        x = self.lstm2(x)
+        x = self.dropout2(x)
+        x = self.lstm3(x)
+        x = self.dense1(x)
+        x = self.dropout3(x)
+        x = self.dense2(x)
+
+        x = self.activation1(x)
+
+        return x
