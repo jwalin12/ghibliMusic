039f5256f879588caef11ba996afbf9d84e8c22d
Jwalin Joshi <jwalin12@berkeley.edu>
2020-12-23 21:03:58 +0530

    added in attention to dev

--- src/network.py
+++ src/network.py
@@ -3,7 +3,8 @@
 
 # Third party import
 import tensorflow as tf
-from tensorflow.keras import Model, Layer
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Layer
 from tensorflow.keras.layers import Activation, Conv2D, Dense, Dropout, Flatten, LSTM
 
 
@@ -12,7 +13,7 @@ from tensorflow.keras.layers import Activation, Conv2D, Dense, Dropout, Flatten,
 
 '''Attention head class. Matrices are trainable variables. Applies Attention Mechanism to
  embedding vector.'''
-class attention_head():
+class attention_head:
     def __init__(self, embedding_length, output_len):
         self.wQ = tf.Variable(tf.random.normal([embedding_length, output_len]))
         self.wK = tf.Variable(tf.random.normal([embedding_length, output_len]))
@@ -31,27 +32,32 @@ class attention_head():
 
 '''Encoder class. Goes through self attention and feed forward neural steps.'''
 class encoder(Layer):
-
     def __init__(self, embedding_length, output_len, nheads = 1, wO_len = 4):
         super(encoder, self).__init__()
 
         self.nheads = nheads
         self.heads = []
-        for _ in range(len(nheads)):
+        for _ in range(nheads):
             self.heads.append(attention_head.__init__(embedding_length, output_len))
         self.wO = None
-        if(self.nheads >1):
+        if (self.nheads >1):
             self.wO = tf.Variable(tf.random.normal([nheads*output_len,wO_len]))
-
 
 '''Does self attention. Normalizes and adds residual. Does Feed Forward Neural Network.
 Adds and normalizes. Assume input is already embedded.'''
     def __call__(self,x):
-        Z = tf.Tensor([], dtype = float)
-
+        Z = tf.Tensor([], dtype = float, value_index=0)
         for head in self.heads:
             tf.concat([Z,head(x)])
-        tf.matmul(Z, self.wO) #attention step
+        atten_to_norm = tf.matmul(Z, self.wO)+ x #attention step
+        atten_normed = tf.keras.layers.LayerNormalization(atten_to_norm) #adding and normalizing
+
+        #TODO: feedforward network
+
+
+
+
+
 
 
 
